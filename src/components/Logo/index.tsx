6f7bc3800e8acd00f8669711b39f4e80e038c9ac
CloverFeng <sunkesi02@gmail.com>
2020-01-25 21:04:34 +0800

    Logo 建立

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'asset/styles/Components/Logo.scss';
+
+const logo: React.FC = () => {
+    return (
+        <div className="t-logo"></div>
+    )
+}
+
+export default logo;
--- src/containers/Toolbar/index.tsx
+++ src/containers/Toolbar/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'asset/styles/Containers/Toolbar.scss';
+import Logo from '../../components/Logo';
 
 const toolbar: React.FC = () => {
     return (
         <div className="t-container">
-            <div>logo</div>
+            <Logo />
             <div>navigation</div>
             <div>shoppingcar</div>
         </div>
