beccd4de94f2584bee8bc50db3ea6a4ea6fd3778
CloverFeng <sunkesi02@gmail.com>
2020-01-26 11:25:02 +0800

    建立 register 文字區塊

---
+++ src/components/Register/RegisterLogo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'asset/styles/Components/Register/RegisterLogo/RegisterLogo.scss';
+
+const registerLogo: React.FC = () => {
+    return (
+        <div className="f-r-l-container">
+            <div className="f-r-l-img"></div>
+            <div className="f-r-l-msg">訂閱你我的甜蜜郵件</div>
+        </div>
+    )
+}
+
+export default registerLogo;
--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'asset/styles/Components/Register/Register.scss';
+import RegisterLogo from '../Register/RegisterLogo';
 
 const register: React.FC = () => {
     return (
         <div className="f-r-container">
-            <div>registerlogo</div>
+            <RegisterLogo />
             <div>inputbox</div>
         </div>
     )
