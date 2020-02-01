0d8ae56e95ad5077e87126370ee87ad837788fc1
CloverFeng <sunkesi02@gmail.com>
2020-02-01 21:29:23 +0800

    完成首頁第2區塊

--- src/components/Home/HomeSectionSecond/index.tsx
+++ src/components/Home/HomeSectionSecond/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import 'asset/styles/Components/Home/HomeSection.scss'
+import 'asset/styles/Components/Home/HomeSectionSecond.scss'
 
-const headerSection: React.FC = () => {
+const homeSectionSecond: React.FC = () => {
     return (
-        <section className="h-s-container">
-            <div className="h-s-layout">
-                <div className="h-s-leftlogo"></div>
-                <div className="h-s-centermsg">
+        <section className="h-s-s-container">
+            <div className="h-s-s-layout">
+                <div className="h-s-s-msg">
                     <p>青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。</p>
                     <p>是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。</p>
                 </div>
-                <div className="h-s-rightlogo"></div>
+                <div className="h-s-s-logo-1"></div>
+                <div className="h-s-s-logo-2"></div>
             </div>
         </section>
     )
 }
 
-export default headerSection;
+export default homeSectionSecond;
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeHeader from '../../components/Home/HomeHeader';
-import HomeSection from '../../components/Home/HomeSection';
+import HomeSectionFirst from '../../components/Home/HomeSectionFirst';
+import HomeSectionSecond from '../../components/Home/HomeSectionSecond';
 
 const home: React.FC = () => {
     return (
         <div>
             <HomeHeader />
-            <HomeSection />
+            <HomeSectionFirst />
+            <HomeSectionSecond />
         </div>
     )
 }
